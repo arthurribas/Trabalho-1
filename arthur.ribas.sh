df3c9d08895c9f3ad1da29492624982120f1b5f3
arthurribas <thurribas@yahoo.com.br>
2016-11-16 20:18:38 -0200

    Add files via upload
    Trabalho 1

---
+++ arthur.ribas.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#Arthur Ribas
+#Orientador: Diego Tumelero
+#Escola Alcides Maya
+
+#Comando para listar interfaces do sistema
+
+ip addr show
+echo "Tecle ENTER para continuar"
+read pause
+clear
+
+echo
+hostname ArthurRibas
+echo
---
+++ testa.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#Arthur Ribas
+#Orientador: Diego Tumelero
+#Escola Alcides Maya
+
+#Mostra versao Ubuntu
+cat /etc/*-release 
+read pause
+
+uname -m
